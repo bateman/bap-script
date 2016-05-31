e02a92e93379c50fdda41f9f651cd6cb01c925cd
Fabio Calefato <fcalefato@gmail.com>
2016-05-31 19:12:59 +0200

    initial work for automatic the tuning of classifiers loaded from models.txt

--- tuning.R
+++ tuning.R
@@ -29,7 +29,7 @@ SO$answers_count <- log1p(SO$longest_sentence)
 SO$answers_count <- log1p(SO$longest_sentence_rank)
 SO$answers_count <- log1p(SO$loglikelihood)
 SO$answers_count <- log1p(SO$loglikelihood_ascending_rank)
-SO$answers_count <- log1p(SO$F.K)
+SO$answers_count <- log1p(abs(SO$F.K))
 SO$answers_count <- log1p(SO$F.K_ascending_rank)
 SO$answers_count <- log1p(SO$has_links)
 
@@ -56,15 +56,27 @@ fitControl <- trainControl(## 10-fold CV
                            # enable parallel computing if avail
                            allowParallel = TRUE)
 
+classifier <- readLines("models.txt")
+for(i in 1:length(classifier)){
+  print(paste("Building model for classifier", classifier[i]))
+  model <- train(solution ~ ., data = training,
+                 method = classifier[i],
+                 trControl = fitControl,
+                 metric = "ROC",
+                 tuneLength = 5 # five values per param
+                 )
+  str(model)
+  rm(model)
+  gc()
+}
 
-model <- train(solution ~ ., data = training,
-               method = "rf",
-               trControl = fitControl,
-               metric = "ROC",
-               tuneLength = 5, # five values per param
-               verbose = FALSE)
-model
+
 
 #p <- predict(model, testing, type="prob")
 #summary(p)
 #plot(p)
+# unload a package:
+## detach("package:vegan", unload=TRUE)
+
+#rm(model)
+#gc()
