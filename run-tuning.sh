0ca3af3f3b666a90d67d08918b2d5b56614bf180
Fabio Calefato <fcalefato@gmail.com>
2016-06-06 13:44:17 +0200

    optimized to have the same output subfolder for each run

--- run-tuning.sh
+++ run-tuning.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 printf " === Starting the tuning of classifiers' params (this may take a while...)\n"
+start_time=$(date +"%Y-%m-%d_%H.%M")
 for i in `seq 1 10`;
 do
         echo " :: Run $i"
-        Rscript tuning.R $i
+        Rscript tuning.R $i $start_time
 done   
 
 echo " Done"
