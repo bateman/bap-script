bb0fa272cdf4a2cac970c0215fb63d93cb04d6f6
Fabio Calefato <fcalefato@gmail.com>
2016-06-18 12:41:47 +0200

    added a test feature file into features/ subfolder

--- feature-selection.R
+++ feature-selection.R
@@ -2,7 +2,7 @@
 args<-commandArgs(TRUE)
 # files with features
 feat_file <- args[1]
-feat_file <- ifelse(is.na(feat_file),"features.csv", feat_file)
+feat_file <- ifelse(is.na(feat_file),"features/test.csv", feat_file)
 # best k features to select, default 10
 k <- args[2]
 k <- ifelse(is.na(k), 10, k)
