2000d7736c8cc56382f67514978508d93eb168bf
Fabio Calefato <fcalefato@gmail.com>
2016-06-19 20:34:47 +0200

    working realease for feature selection with Boruta and FSelector packages

--- feature-selection.R
+++ feature-selection.R
@@ -9,40 +9,51 @@ k <- ifelse(is.na(k), 10, k)
 
 # load the feature file into dataframe dfm 
 dfm <- read.csv(feat_file, header = TRUE)
+
 # convert boolean factors 
 dfm$has_links<- as.integer(as.logical(dfm$has_links))
-# remove answer_uid
+dfm$solution<- as.integer(as.logical(dfm$solution))
+# remove answer id (useless)
 dfm <- dfm[ , !(names(dfm) %in% c("answer_uid"))]
+# first convert timestamps into POSIX std time values, then to equivalent number
+dfm$date_time <- as.numeric(as.POSIXct(strptime(dfm$date_time, tz="CET", "%Y-%m-%d %H:%M:%S")))
+# exclude rows with NaN (missing values)
+dfm <- na.omit(dfm)
 
-# first convert timestamps into POSIX std time values
-dfm$date_time <- as.numeric(as.POSIXct(dfm$date_time, format = "'%Y-%m-%d %H:%M:%S'")) # then to equivalent number
+# output file for the classifier at nad
+output_file <- paste("output", "feature-subset.txt", sep = "/")
 
 # feature selection through Wrapper algorithm
 library("Boruta")
 # default pValue = 0.01 is used
 # try to reduce maxRuns if it takes too long
-b.fs <- Boruta(solution~., data=dfm, maxRuns = 100, doTrace = 0, holdHistory = TRUE)
-
-print(b.fs)
-plot(b.fs, sort=TRUE)
-plotImpHistory(b.fs)
+b.fs <- Boruta(solution~., data=dfm, maxRuns = 100, doTrace = 2, holdHistory = TRUE)
+out <- capture.output(b.fs)
+cat("*******  BORUTA  *******", out, file=output_file, sep="\n", append=TRUE)
+#plot(b.fs, sort=TRUE)
+#plotImpHistory(b.fs)
 
 
 # feature selection through CFS - Correlation Feature Selection
 library("FSelector")
 
-# use Pearson’s correlation, requires all data to be continous
+# use Pearson's correlation, requires all data to be continous
 weights <- linear.correlation(solution~., data=dfm)
-print(weights)
+out <- capture.output(weights)
+cat("\n*******  CFS Pearson's correlation  *******", out, file=output_file, sep="\n", append=TRUE)
 # select K best attributes
 subset <- cutoff.k(weights, k)
-f <- as.simple.formula(subset, "solution")
-print(f)
-
-# use Spearman’s ro correlation, requires all data to be continous
+out <- capture.output(subset)
+cat("", out, file=output_file, sep="\n", append=TRUE)
+#f <- as.simple.formula(subset, "solution")
+#print(f)
+# use Spearman's ro correlation, requires all data to be continous
 weights <- rank.correlation(solution~., data=dfm)
-print(weights)
+out <- capture.output(weights)
+cat("\n*******  CFS Spearman’s ro correlation  *******", out, file=output_file, sep="\n", append=TRUE)
 # select K best attributes
 subset <- cutoff.k(weights, k)
-f <- as.simple.formula(subset, "solution")
-print(f)
+out <- capture.output(subset)
+cat("", out, file=output_file, sep="\n", append=TRUE)
+#f <- as.simple.formula(subset, "solution")
+#print(f)
