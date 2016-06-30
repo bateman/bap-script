4ca945141486f9db996e55158347b8f8891ddc19
Fabio Calefato <fcalefato@gmail.com>
2016-06-30 18:44:10 +0200

    now plots are square-sized and axes have same min and max values

--- lib/plot_curve.R
+++ lib/plot_curve.R
@@ -3,19 +3,21 @@
 library(ROCR)  # for plotting ROC curves
 
 plot_curve <- function(predictions, classifiers, colors, line_types=1, line_widths=1, x_label="fpr", y_label="tpr",
-                       leg_pos="bottomright"){
+                       leg_pos="bottomright", plot_abline=TRUE){
   par(
     mar = c(5, 5, 2, 2),
     xaxs = "i",
     yaxs = "i",
     cex.axis = 1,
-    cex.lab = 1.1
+    cex.lab = 1.1,
+    pty="s" # square-sized plot
   )
+  range = c(0, 1.0)
   
   for(i in 1:length(predictions)) {
     adding <- ifelse(i == 1, FALSE, TRUE)
     perf <- performance(predictions[[i]], y_label, x_label)
-    plot(perf, add = adding, col = colors,  lty = line_types[i], lwd = line_widths)
+    plot(perf, add = adding, col = colors,  lty = line_types[i], lwd = line_widths, xlim=range, ylim=range)
   }
   
   # Add a legend
@@ -32,5 +34,6 @@ plot_curve <- function(predictions, classifiers, colors, line_types=1, line_widt
   )
   
   # and random guess line
-  abline(a = 0, b = 1, lty = "dotted", lwd = 0.3, col="grey")
+  if(plot_abline == TRUE)
+    abline(a = 0, b = 1, lty = "dotted", lwd = 0.3, col="grey")
 }
