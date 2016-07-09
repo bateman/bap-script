029a2673aaf3178049688708366cce8a46dcfbc5
Fabio Calefato <fcalefato@gmail.com>
2016-07-09 20:03:49 +0200

    extracted function to load dataframes from csv

---
+++ lib/setup_dataframe.R
@@ -0,0 +1,31 @@
+
+setup_dataframe <- function(dataframe, outcomeName, excluded_predictors, time_format="%Y-%m-%d %H:%M:%S", 
+                            normalize=TRUE, na_omit=TRUE) {
+  dataframe <- dataframe[ , !(names(dataframe) %in% excluded_predictors)]
+  predictorsNames <- names(dataframe[,!(names(dataframe)  %in% c(outcomeName))]) # removes the var to be predicted from the test set
+  
+  # convert boolean factors 
+  dataframe$has_links<- as.integer(as.logical(dataframe$has_links))
+  
+  # first check whether thera are leading and trailing apostrophes around the date_time field
+  dataframe$date_time <- gsub("'", '', dataframe$date_time)
+  # then convert timestamps into POSIX std time values, then to equivalent numbers
+  dataframe$date_time <- as.numeric(as.POSIXct(strptime(dataframe$date_time, tz="CET", time_format)))
+  
+  if(normalize == TRUE) {
+    if(!require(e1071))
+      library(e1071) # for normality adjustment
+    # normality adjustments for indipendent vars (predictors)
+    # ln(x+1) transformation mitigates skeweness
+    for (i in 1:length(predictorsNames)){
+      dataframe[,predictorsNames[i]] <- log1p(dataframe[,predictorsNames[i]])
+    }
+  }
+  
+  if(na_omit == TRUE) {
+    # exclude rows with Na, NaN and Inf (missing values)
+    dataframe <- na.omit(dataframe)
+  }
+  
+  return(list(dataframe, predictorsNames))
+}
--- test.R
+++ test.R
@@ -9,38 +9,9 @@ if(!exists("save_results", mode="function"))
   source(paste(getwd(), "lib/save_results.R", sep="/"))
 if(!exists("scalar_metrics", mode="function")) 
   source(paste(getwd(), "lib/scalar_metrics.R", sep="/"))
+if(!exists("setup_dataframe", mode="function")) 
+  source(paste(getwd(), "lib/setup_dataframe.R", sep="/"))
 
-
-setup_dataframe <- function(dataframe, outcomeName, excluded_predictors, time_format="%Y-%m-%d %H:%M:%S", 
-                            normalize=TRUE, na_omit=TRUE) {
-  dataframe <- dataframe[ , !(names(dataframe) %in% excluded_predictors)]
-  predictorsNames <- names(dataframe[,!(names(dataframe)  %in% c(outcomeName))]) # removes the var to be predicted from the test set
-  
-  # convert boolean factors 
-  dataframe$has_links<- as.integer(as.logical(dataframe$has_links))
-  
-  # first check whether thera are leading and trailing apostrophes around the date_time field
-  dataframe$date_time <- gsub("'", '', dataframe$date_time)
-  # then convert timestamps into POSIX std time values, then to equivalent numbers
-  dataframe$date_time <- as.numeric(as.POSIXct(strptime(dataframe$date_time, tz="CET", time_format)))
-  
-  if(normalize == TRUE) {
-    if(!require(e1071))
-      library(e1071) # for normality adjustment
-    # normality adjustments for indipendent vars (predictors)
-    # ln(x+1) transformation mitigates skeweness
-    for (i in 1:length(predictorsNames)){
-      dataframe[,predictorsNames[i]] <- log1p(dataframe[,predictorsNames[i]])
-    }
-  }
-  
-  if(na_omit == TRUE) {
-    # exclude rows with Na, NaN and Inf (missing values)
-    dataframe <- na.omit(dataframe)
-  }
-  
-  return(list(dataframe, predictorsNames))
-}
 
 # name of outcome var to be predicted
 outcomeName <- "solution"
